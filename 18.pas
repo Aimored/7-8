b6913de51fe1830384197d4b3cfeed5c38af7b66
Aimored <144654878+Aimored@users.noreply.github.com>
2023-11-28 15:07:06 +0300

    Add files via upload

---
+++ 18.pas
@@ -0,0 +1,16 @@
+﻿var
+str: string;
+count,i: integer;
+begin
+writeln('Введите строку:');
+readln(str);
+
+count := 0;
+for i := 1 to length(str) - 2 do
+begin
+if (str[i] = 'a') and (str[i + 1] = 'b') and (str[i + 2] = 'a') then
+count := count + 1;
+end;
+
+writeln('Количество вхождений "aba": ', count);
+end.
